7c9380b8012c3617d9bacd0cd841cbbcec250b02
Iwegbu Victor <iwegbuvc@gmail.com>
2023-10-01 04:05:39 +0100

    added some validation

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
-let isGame = true;
-const randomNumber =  Math.floor((Math.random() * 100) + 1);
-let userName;
-
-userName = prompt("Welcome to the guessing game, \nWhat is your name?")
-
-
-const level = parseInt(prompt("Enter your level\n1 = Begginer\n2 = Intermediate\n3 = Advanced"))
-
-function calcLive() {
-    return level == 1 ? 5
-      : level == 2 ? 3
-      : level == 3 ? 2
-      : 5;
+let level;
+// giving user chance to choose gameLevel
+while (true) {
+  level = parseInt(
+    prompt(
+      "Hi there welcome!\nChoose your level\n1=Beginner\n2=Intermediate\n3=Advance"
+    )
+  );
+  if (isNaN(level) || level < 1 || level > 3) {
+    alert("Wrong value\nPlease input correctly your level");
+  } else {
+    break;
   }
-  
-let live = calcLive()
-console.log(`You have ${live} lives`);
-
-//console.log(randomNumber);
-
-while (isGame & live > 0){
-    const userGuess = parseInt(prompt("Enter a number between 1 and 100"))
-
-    console.log(userGuess);
-
-    if (userGuess === randomNumber){
-        console.log(`You guessed the number right, You are doing good, ${userName}`)
-        break;
-        
-    } else if (userGuess > randomNumber){
-        console.log(`Come on ${userName} You guessed too high, try again`)
-    } else if (userGuess < randomNumber){
-        console.log( `Opp!! ${userName} You guessed too low try again!`)
-    }
-    
-    live--
-    console.log(`You have ${live} lives left, ${userName}`)
-
 }
 
+// Calculating users chances based on the level entered
+function calculateLives() {
+  if (level == 1) {
+    return 5;
+  } else if (level == 2) {
+    return 3;
+  } else if (level == 3) {
+    return 1;
+  }
+  return 0;
+}
 
+let gameLives = calculateLives();
+
+// generating random number
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+// user guessing random number
+let userGuess;
+while (true && gameLives > 0) {
+  // Use a while loop to ensure valid input
+  while (true) {
+    let userInput = prompt(
+      `Guess a number between 1 to 10, You have ${gameLives} Chance/s`
+    );
+    if (userInput == null) {
+      break;
+    } else if (isNaN(userInput) || userInput < 1 || userInput > 10) {
+      alert("Choose a valid number");
+    } else {
+      userGuess = parseInt(userInput);
+      break;
+    }
+  }
+
+  // logical checks
+  if (userGuess == randomNumber) {
+    alert("Congrats! You guess correctly");
+    break;
+  } else if (userGuess > randomNumber) {
+    gameLives--;
+    alert(
+      `Opps!\n Your guess is too high, make another guess and you have ${gameLives} trial left`
+    );
+  } else if (userGuess < randomNumber) {
+    gameLives--;
+    alert(
+      `Sorry your guess is low\n make another guess and you have ${gameLives} trial left`
+    );
+  } else if (userGuess == null) {
+    break;
+  }
+}
